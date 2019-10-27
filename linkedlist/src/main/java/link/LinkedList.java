f916215f44f906aae032a310b521dc13866bde43
yangsongbai <yangsongbaivat@163.com>
2019-10-27 17:27:12 +0800

    链表删除结点

--- linkedlist/src/main/java/link/LinkedList.java
+++ linkedlist/src/main/java/link/LinkedList.java
@@ -1,5 +1,10 @@
 package link;
 
+/**
+ * addLast(e) O(n)
+ *
+ * @param <E>
+ */
 public class LinkedList<E> {
   private Node dummyHead;
   private int size;
@@ -22,6 +27,27 @@ public class LinkedList<E> {
     node.next = head;
     head = node;*/
     add(0, e);
+  }
+
+  public E remove(int index) {
+    if (index < 0 || index > size) throw new IllegalArgumentException("");
+    Node prev = dummyHead;
+    for (int i = 0; i < index - 1; i++) {
+      prev = prev.next;
+    }
+    Node retNode = prev.next;
+    prev.next = retNode.next;
+    retNode.next = null;
+    size--;
+    return retNode.e;
+  }
+
+  public E removeFirst() {
+    return remove(0);
+  }
+
+  public E removeLast() {
+    return remove(size);
   }
 
   public void add(int index, E e) {
